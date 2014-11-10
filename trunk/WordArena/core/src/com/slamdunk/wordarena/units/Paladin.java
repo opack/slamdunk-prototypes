d39b034c2dee0c3cd8d252ba8d1f5acff37dda93
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2014-11-10 23:27:22 +0000

    ADD : Animation du paladin

--- trunk/WordArena/core/src/com/slamdunk/wordarena/units/Paladin.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/units/Paladin.java
@@ -1,5 +1,7 @@
 package com.slamdunk.wordarena.units;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.slamdunk.toolkit.graphics.drawers.AnimationCreator;
 import com.slamdunk.wordarena.screens.GameScreen;
 
 public class Paladin extends OffensiveUnit {
@@ -7,6 +9,12 @@ public class Paladin extends OffensiveUnit {
 		super(game, Factions.PLAYER, "paladin.png", 1, 1, 2, 0.5f);
 		setHp(10);
 		setSpeed(2);
+		
+		Animation animation = AnimationCreator.create("textures/warrior_m.png", 3, 4, 0.25f, 6, 7, 8);
+		createDrawers(false, true, false);
+		getTextureDrawer().setActive(false);
+		getAnimationDrawer().setAnimation(animation, true, true);
+		
 	}
 	
 	@Override
