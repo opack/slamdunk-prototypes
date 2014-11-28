ff4c78edbffeb74c2748fe32706cfda25f6ecc52
Opack <opack@users.noreply.github.com>
2014-11-28 17:33:19 +0000

    DEV : PathList depuis SVG

--- WordArena/core/src/com/slamdunk/wordarena/screens/PathTest.java
+++ WordArena/core/src/com/slamdunk/wordarena/screens/PathTest.java
@@ -43,14 +43,51 @@ public class PathTest extends SlamScreen {
 //			new Vector2(340, 680),
 //			new Vector2(120, 680)
 //		);
-		path = new PathList<Vector2>(
-				new Vector2[]{new Vector2(120, 400), new Vector2(120, 80), new Vector2(690, 80)},
-				new Vector2[]{new Vector2(690, 80), new Vector2(470, 270)},
-				new Vector2[]{new Vector2(470, 270), new Vector2(340, 460), new Vector2(340, 680)},
-				new Vector2[]{new Vector2(340, 680), new Vector2(120, 400)}
-			);
-		cursor = new PathListCursor<Vector2>(path, speed, CursorMode.LOOP_PINGPONG);
+//		path = new PathList<Vector2>(
+//			new Vector2[]{new Vector2(120, 400), new Vector2(120, 80), new Vector2(690, 80)},
+//			new Vector2[]{new Vector2(690, 80), new Vector2(470, 270)},
+//			new Vector2[]{new Vector2(470, 270), new Vector2(340, 460), new Vector2(340, 680)},
+//			new Vector2[]{new Vector2(340, 680), new Vector2(120, 400)}
+//		);
+		
+//		path = new PathList<Vector2>(
+//			/**
+//			 * m 152.85715,561.85715
+//			 * 		c
+//			 * 			0,0
+//			 * 			17.14286,-114.28572
+//			 * 			105.71428,-168.57143
+//			 */
+//			new Vector2[]{transform(152.85715f,561.85715f), transform(152.85715f,561.85715f), transform(170.00001f,447.57143f), transform(275.71429f,279f)},
+//			/**
+//			 * 			88.57143,-54.28572
+//			 * 			190,-82.85714
+//			 * 			190,-82.85714
+//			 */
+//			new Vector2[]{transform(275.71429f,279f), transform(364.28572f,224.71428f), transform(557.28572f,141.85714f), transform(744.28572f,59f)},
+//			/**
+//			 * 			0,0
+//			 * 			57.14286,-22.85715
+//			 * 			455.71429,-98.57144"
+//			 */
+//			new Vector2[]{transform(744.28572f,59f), transform(744.28572f,59f), transform(801.42858f,36.14285f), transform(1257.14287f,-62.42859f)}
+//		);
+		
+		//M 158.57143,599.50504 C 191.32127,504.64831 264.52851,426.57881 354.60035,383.27495
+		path = PathList.createCubicBezierPathList(2,
+			158.57143f,599.50504f, 191.32127f,504.64831f, 264.52851f,426.57881f, 354.60035f,383.27495f,
+			545.32269f,297.00714f, 732.99567f,299.73707f, 914.28571f,252.36218f);
+		
+		parse("battlefields/battlefield0_simple.svg");
+		cursor = new PathListCursor<Vector2>(path, speed, CursorMode.LOOP);
 		Gdx.input.setInputProcessor(this);
+	}
+	
+	private Vector2 transform(float x, float y) {
+		return new Vector2(x / 2, 300 - y / 2);
+	}
+
+	private void parse(String string) {
 	}
 
 	final Vector2 computedPosition = new Vector2();
