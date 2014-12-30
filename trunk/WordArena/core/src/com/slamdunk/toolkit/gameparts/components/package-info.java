760b1f7495699e169c24f8149c961ab733f548b6
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2014-12-30 11:03:28 +0000

    MOD : Répartition des composants en Parts et Scripts

--- trunk/WordArena/core/src/com/slamdunk/toolkit/gameparts/components/package-info.java
+++ trunk/WordArena/core/src/com/slamdunk/toolkit/gameparts/components/package-info.java
@@ -4,9 +4,11 @@
  *   - Les parties (-Part) : représentent un morceau "tangible" du GameObject.
  *   Les Parts contiennent essentiellement des données et ont une représentation
  *   directe et concrète sur le monde, ce qui implique qu'elles sont essentiellement
- *   les seuls composants à implémenter la méthode render().
+ *   les seuls composants à implémenter la méthode render(). Pour résumer, les
+ *   Parts "sont" quelque chose.
  *   - Les scripts (-Script) : représentent un comportement, une logique
  *   applicative du GameObject. Les Scripts contiennent essentiellement
- *   des algos de manipulation de Parts.
+ *   des algos de manipulation de Parts. Pour résumer, les
+ *   Scripts "font" quelque chose.
  */
 package com.slamdunk.toolkit.gameparts.components;
