6f80ced9825190507aad03f4698c21c9e8438497
Opack <opack@users.noreply.github.com>
2014-12-31 13:38:46 +0000

    ADD : UIComponent.updateWidget MOD : UIButtonPart, UISliderPart : Override de updateWidget

--- WordArena/core/src/com/slamdunk/toolkit/gameparts/components/ui/UISliderPart.java
+++ WordArena/core/src/com/slamdunk/toolkit/gameparts/components/ui/UISliderPart.java
@@ -6,7 +6,12 @@ public class UISliderPart extends UIComponent {
 	public float minValue;
 	public float currentValue;
 	public float maxValue;
-	public float stepValue;
+	public float stepSize;
+	
+	/**
+	 * Utilisée uniquement lors de la création du widget,
+	 * non modifiable à runtime.
+	 */
 	public boolean verticalOriented;
 	
 	private Slider slider;
@@ -16,13 +21,13 @@ public class UISliderPart extends UIComponent {
 		minValue = 0;
 		currentValue = 0;
 		maxValue = 100;
-		stepValue = 1;
+		stepSize = 1;
 		verticalOriented = false;
 	}
 	
 	@Override
 	public void init() {
-		slider = new Slider(minValue, maxValue, stepValue, verticalOriented, skin);
+		slider = new Slider(minValue, maxValue, stepSize, verticalOriented, skin);
 		slider.setValue(currentValue);
 		actor = slider;
 		super.init();
@@ -32,5 +37,20 @@ public class UISliderPart extends UIComponent {
 	public void update(float deltaTime) {
 		super.update(deltaTime);
 		currentValue = slider.getValue();
+	}
+
+	@Override
+	public void updateWidget() {
+		// Attention ! L'ordre de ces méthodes est important !
+		if (slider.getStepSize() != stepSize) {
+			slider.setStepSize(stepSize);
+		}
+		if (slider.getMinValue() != minValue
+		|| slider.getMaxValue() != maxValue) {
+			slider.setRange(minValue, maxValue);
+		}
+		if (slider.getValue() != currentValue) {
+			slider.setValue(currentValue);
+		}
 	}
 }
