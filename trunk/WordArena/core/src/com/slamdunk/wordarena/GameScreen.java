24b51846e7136808c7870b327228e045902862e4
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2015-01-08 22:53:29 +0000

    DEV

--- trunk/WordArena/core/src/com/slamdunk/wordarena/GameScreen.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/GameScreen.java
@@ -1,22 +1,8 @@
-/*******************************************************************************
- * Copyright 2011 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
 package com.slamdunk.wordarena;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;
 import com.slamdunk.wordarena.systems.BoundsSystem;
 import com.slamdunk.wordarena.systems.RenderingSystem;
@@ -34,7 +20,11 @@ public class GameScreen extends ScreenAdapter {
 		engine.addSystem(new BoundsSystem());
 		engine.addSystem(new RenderingSystem(game.batcher));
 		
+		input
+		
 		ui = new GameUI(this);
+		
+		Gdx.input.setInputProcessor(new InputMultiplexer(this, ui.getStage()));
 		
 		loadNextLevel();
 		
