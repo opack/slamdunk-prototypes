7a1d447102cba28b8d4a4c8ca7c3b4c0d71bb90f
Opack <opack@users.noreply.github.com>
2015-01-09 23:48:05 +0000

    ADD : InputSystem

--- WordArena/core/src/com/slamdunk/wordarena/Arena.java
+++ WordArena/core/src/com/slamdunk/wordarena/Arena.java
@@ -5,89 +5,95 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.MathUtils;
 import com.slamdunk.wordarena.components.BoundsComponent;
 import com.slamdunk.wordarena.components.CameraComponent;
-import com.slamdunk.wordarena.components.CellComponent;
+import com.slamdunk.wordarena.components.InputComponent;
+import com.slamdunk.wordarena.components.LetterCellComponent;
 import com.slamdunk.wordarena.components.TextureComponent;
 import com.slamdunk.wordarena.components.TransformComponent;
 import com.slamdunk.wordarena.systems.RenderingSystem;
 
 public class Arena {
-	public static final float ARENA_WIDTH = 25;
-	public static final float ARENA_HEIGHT = 15;
-	public static final int ARENA_STATE_RUNNING = 0;
-	public static final int ARENA_STATE_NEXT_LEVEL = 1;
-	public static final int ARENA_STATE_GAME_OVER = 2;
-
+	private int width;
+	private int height;
+	
 	public int score;
-	public int state;
+	public GameStates state;
 	private Engine engine;
 
-	public Arena(Engine engine) {
+	public Arena(Engine engine, int width, int height) {
 		this.engine = engine;
+		this.width = width;
+		this.height = height;
 
 		createBackground();
 		createArena();
 		createCamera();
 
 		this.score = 0;
-		this.state = ARENA_STATE_RUNNING;
+		this.state = GameStates.RUNNING;
 	}
 	
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
 	private void createArena() {
-		for (int y = 0; y < ARENA_HEIGHT; y++) {
-			for (int x = 0; x < ARENA_WIDTH; x++) {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
 				createCell(CellStates.NORMAL, x, y);
 			}
 		}
 	}
 	
 	private void createCell(CellStates type, float x, float y) {
-		Entity entity = new Entity();
+		TransformComponent transform = new TransformComponent();
+		transform.pos.set(x, y, Layers.LETTERS.ordinal());
 		
-		CellComponent cell = new CellComponent();
 		BoundsComponent bounds = new BoundsComponent();
-		TransformComponent position = new TransformComponent();
-		TextureComponent texture = new TextureComponent();
-		
-		cell.type = type;
-		cell.letter = Letters.values()[MathUtils.random(25)];
-		
-		texture.region = Assets.letterData.get(cell.letter, cell.type);
-		
 		bounds.bounds.width = 1;
 		bounds.bounds.height = 1;
 		
-		position.pos.set(x, y, Layers.CELLS.ordinal());
+		InputComponent input = new InputComponent();
 		
-		entity.add(cell);
-		entity.add(bounds);
-		entity.add(position);
+		LetterCellComponent letterCell = new LetterCellComponent();
+		letterCell.type = type;
+		letterCell.letter = Letters.values()[MathUtils.random(25)];
+		
+		TextureComponent texture = new TextureComponent();
+		texture.region = Assets.letterData.get(letterCell.letter, letterCell.type);
+		
+		Entity entity = new Entity();
+		entity.add(transform);
 		entity.add(texture);
+		entity.add(input);
+		entity.add(letterCell);
+		entity.add(bounds);
 		
 		engine.addEntity(entity);
 	}
 	
 	private void createCamera() {
-		Entity entity = new Entity();
-		
 		CameraComponent camera = new CameraComponent();
 		camera.camera = engine.getSystem(RenderingSystem.class).getCamera();
 		
+		Entity entity = new Entity();
 		entity.add(camera);
 		
 		engine.addEntity(entity);
 	}
 	
 	private void createBackground() {
-		Entity entity = new Entity();
+		TransformComponent transform = new TransformComponent();
+		transform.pos.set(0, 0, Layers.BACKGROUND.ordinal());
 		
-		TransformComponent position = new TransformComponent();
 		TextureComponent texture = new TextureComponent();
-		
 		texture.region = Assets.backgroundRegion;
 		
-		position.pos.set(0, 0, Layers.BACKGROUND.ordinal());
-		
-		entity.add(position);
+		Entity entity = new Entity();
+		entity.add(transform);
 		entity.add(texture);
 		
 		engine.addEntity(entity);
--- WordArena/core/src/com/slamdunk/wordarena/systems/BoundsSystem.java
+++ WordArena/core/src/com/slamdunk/wordarena/systems/BoundsSystem.java
@@ -1,22 +1,5 @@
-/*******************************************************************************
- * Copyright 2014 See AUTHORS file.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- ******************************************************************************/
-
 package com.slamdunk.wordarena.systems;
 
-import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
@@ -25,23 +8,17 @@ import com.slamdunk.wordarena.components.TransformComponent;
 
 public class BoundsSystem extends IteratingSystem {
 	
-	private ComponentMapper<TransformComponent> tm;
-	private ComponentMapper<BoundsComponent> bm;
-	
 	@SuppressWarnings("unchecked")
 	public BoundsSystem() {
 		super(Family.all(BoundsComponent.class, TransformComponent.class).get());
-		
-		tm = ComponentMapper.getFor(TransformComponent.class);
-		bm = ComponentMapper.getFor(BoundsComponent.class);
 	}
 
 	@Override
 	public void processEntity(Entity entity, float deltaTime) {
-		TransformComponent pos = tm.get(entity);
-		BoundsComponent bounds = bm.get(entity);
+		TransformComponent transform = ComponentMappers.TRANSFORM.get(entity);
+		BoundsComponent bounds = ComponentMappers.BOUNDS.get(entity);
 		
-		bounds.bounds.x = pos.pos.x - bounds.bounds.width / 2;
-		bounds.bounds.y = pos.pos.y - bounds.bounds.height / 2;
+		bounds.bounds.x = transform.pos.x;// - bounds.bounds.width / 2;
+		bounds.bounds.y = transform.pos.y;// - bounds.bounds.height / 2;
 	}
 }
