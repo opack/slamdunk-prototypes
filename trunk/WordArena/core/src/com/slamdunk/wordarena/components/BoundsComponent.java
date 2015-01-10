d2e9cef346f4ff2342e9e3a5ff8f8e83aaca426a
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2015-01-10 09:35:44 +0000

    ADD : Commentaires

--- trunk/WordArena/core/src/com/slamdunk/wordarena/components/BoundsComponent.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/components/BoundsComponent.java
@@ -3,6 +3,10 @@ package com.slamdunk.wordarena.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Rectangle;
 
+/**
+ * Contient les bornes de l'entité, donc sa taille et sa position (position mise à
+ * jour par le BoundsSystem en utilisant le TransformComponent)
+ */
 public class BoundsComponent extends Component {
 	public final Rectangle bounds = new Rectangle();
 }
--- trunk/WordArena/core/src/com/slamdunk/wordarena/components/ColliderComponent.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/components/ColliderComponent.java
@@ -4,6 +4,18 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ * Définit une zone de collision qui peut être utilisée pour des
+ * besoins variés (détection de touche, collision entre objets du
+ * jeu...).
+ * Une zone de collision est composée de bornes dont on définit la
+ * taille et dont la position sera ajustée par le ColliderSystem
+ * en fonction :
+ *   - du TransformComponent de l'entité
+ *   - de la relativeOrigin définie dans le collider et permettant
+ *   de positionner la zone de collision par rapport à la position
+ *   de l'entité (comme un offset)
+ */
 public class ColliderComponent extends Component {
 	/**
 	 * Décalage par rapport au x;y de l'entité
