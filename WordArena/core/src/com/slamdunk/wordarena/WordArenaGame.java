f5f55f629a228f07c537600d79f1836c98f3c412
Opack <opack@users.noreply.github.com>
2015-02-06 10:09:33 +0000

    ADD : Affichage de la grille avec gestion des bordures

--- WordArena/core/src/com/slamdunk/wordarena/WordArenaGame.java
+++ WordArena/core/src/com/slamdunk/wordarena/WordArenaGame.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.slamdunk.toolkit.screen.SlamGame;
 import com.slamdunk.toolkit.settings.SlamSettings;
-import com.slamdunk.wordarena.Assets;
+import com.slamdunk.wordarena.screens.arena.ArenaScreen;
 
 public class WordArenaGame extends SlamGame {
 	public static final int SCREEN_WIDTH = 800;
