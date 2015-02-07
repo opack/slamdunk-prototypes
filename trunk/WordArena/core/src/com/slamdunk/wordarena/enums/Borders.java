88e5a32abbb7c189629768ccfff84b8488d0ae2d
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2015-02-07 10:40:30 +0000

    DEL : Enum Orientation

--- trunk/WordArena/core/src/com/slamdunk/wordarena/enums/Borders.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/enums/Borders.java
@@ -4,18 +4,8 @@ package com.slamdunk.wordarena.enums;
  * Détaille les côtés d'une cellule
  */
 public enum Borders {
-	TOP(Orientation.HORIZONTAL),
-	RIGHT(Orientation.VERTICAL),
-	BOTTOM(Orientation.HORIZONTAL),
-	LEFT(Orientation.VERTICAL);
-	
-	private Orientation orientation;
-
-	private Borders(Orientation orientation) {
-		this.orientation = orientation;
-	}
-	
-	public Orientation getOrientation() {
-		return orientation;
-	}
+	TOP,
+	RIGHT,
+	BOTTOM,
+	LEFT;
 }
