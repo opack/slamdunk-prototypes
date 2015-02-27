1e9081f492e83c08a89e27c0d925ceebafd99d8f
Opack <Opack@0dc0a7a4-2668-4fbe-b320-9fc3ded8d574>
2015-02-27 14:26:17 +0000

    DEV

--- trunk/WordArena/core/src/com/slamdunk/wordarena/screens/home/HomeUI.java
+++ trunk/WordArena/core/src/com/slamdunk/wordarena/screens/home/HomeUI.java
@@ -1,6 +1,8 @@
 package com.slamdunk.wordarena.screens.home;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.slamdunk.toolkit.screen.overlays.UIOverlay;
@@ -20,6 +22,9 @@ public class HomeUI extends UIOverlay {
 		
 		// Charge les éléments de la scène Overlap2D
 		loadScene();
+		
+		// Charge la liste des parties en cours
+		loadCurrentGames();
 	}
 
 	private void loadScene() {
@@ -53,5 +58,26 @@ public class HomeUI extends UIOverlay {
 		sceneLoader.sceneActor.getCompositeById("btnArena1").addScript(new GameLaunchScript(screen));
 		sceneLoader.sceneActor.getCompositeById("btnArena2").addScript(new GameLaunchScript(screen));
 		sceneLoader.sceneActor.getCompositeById("btnArena3").addScript(new GameLaunchScript(screen));
+	}
+	
+	private void loadCurrentGames() {
+		// Créer une table
+		// ...
+		
+		// Créer la ligne de boutons pour changer le type de partie
+		// ...
+		
+		// Charger les parties en cours pour le premier type
+		// ...
+		
+		// Ajouter ces parties à la table (1 partie par ligne, 1 ligne d'en-tête par type de partie)
+		// ...
+		
+		// Placer la table dans un ScrollPane pour permettre le scroll
+		ScrollPane scrollPane = new ScrollPane(list, Assets.skin);
+		scrollPane.setFlickScroll(false);
+		
+		// Ajouter le ScrollPane au Stage
+		getStage().addActor(scrollPane);
 	}
 }
